6ae4bb0954971790fe6ccfc542ccfaa956665b62
Uri Shtand <uri.shtand@gmail.com>
2019-07-07 21:03:20 +0300

    Add spring-boot application with vaadin

---
+++ spring-desktop-app/src/main/java/com/shtand/imageanalyzer/springdesktopapp/SpringDesktopAppApplication.java
@@ -0,0 +1,13 @@
+package com.shtand.imageanalyzer.springdesktopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDesktopAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDesktopAppApplication.class, args);
+    }
+
+}
---
+++ spring-desktop-app/src/test/java/com/shtand/imageanalyzer/springdesktopapp/SpringDesktopAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shtand.imageanalyzer.springdesktopapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDesktopAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
