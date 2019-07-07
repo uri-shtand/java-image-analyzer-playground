d1fa8cdc80c8c0a5537a253df677f1762414ae00
Uri Shtand <uri.shtand@gmail.com>
2019-07-07 22:30:22 +0300

    Start playing with OpenCV

--- analyzer-logic/src/test/java/com/shtand/image/analyzer/logic/opencv/OpenCvPlaygroundTest.java
+++ analyzer-logic/src/test/java/com/shtand/image/analyzer/logic/opencv/OpenCvPlaygroundTest.java
@@ -1,9 +1,12 @@
 package com.shtand.image.analyzer.logic.opencv;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.opencv.core.Mat;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 class OpenCvPlaygroundTest {
 
@@ -15,6 +18,9 @@ class OpenCvPlaygroundTest {
     }
 
     @Test
-    public void testDoNothing() {
+    public void testLoadImage() throws IOException {
+        InputStream resourceAsStream = OpenCvPlaygroundTest.class.getResourceAsStream("/images_nohead/1.jpg");
+        Mat mat = openCvPlayground.loadImageToMatrix(IOUtils.toByteArray(resourceAsStream));
+        System.out.println(mat.height());
     }
 }
