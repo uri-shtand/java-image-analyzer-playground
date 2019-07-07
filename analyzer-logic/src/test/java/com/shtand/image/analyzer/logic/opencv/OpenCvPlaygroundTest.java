877c7227201db2928dc641539f25ec2f78287774
Uri Shtand <uri.shtand@gmail.com>
2019-07-07 23:33:24 +0300

    Start playing with OpenCV

--- analyzer-logic/src/test/java/com/shtand/image/analyzer/logic/opencv/OpenCvPlaygroundTest.java
+++ analyzer-logic/src/test/java/com/shtand/image/analyzer/logic/opencv/OpenCvPlaygroundTest.java
@@ -4,6 +4,8 @@ import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.opencv.core.Mat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,7 +13,7 @@ import java.io.InputStream;
 class OpenCvPlaygroundTest {
 
     private OpenCvPlayground openCvPlayground;
-
+    private static final Logger log = LoggerFactory.getLogger(OpenCvPlaygroundTest.class);
     @BeforeEach
     void setUp() {
         openCvPlayground = new OpenCvPlayground();
@@ -21,6 +23,7 @@ class OpenCvPlaygroundTest {
     public void testLoadImage() throws IOException {
         InputStream resourceAsStream = OpenCvPlaygroundTest.class.getResourceAsStream("/images_nohead/1.jpg");
         Mat mat = openCvPlayground.loadImageToMatrix(IOUtils.toByteArray(resourceAsStream));
-        System.out.println(mat.height());
+        log.info("Mat has {} height {} width and {} depth", mat.height(), mat.width(), mat.depth());
+        log.info("Mat has {} total {} channels and {} dims", mat.total(), mat.channels(), mat.dims());
     }
 }
