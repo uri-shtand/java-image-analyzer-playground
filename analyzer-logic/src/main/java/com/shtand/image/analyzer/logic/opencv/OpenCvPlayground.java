7b21e23b7349d85224eb92a618662747c7b19470
Uri Shtand <uri.shtand@gmail.com>
2019-07-07 22:25:33 +0300

    Start playing with OpenCV

--- analyzer-logic/src/main/java/com/shtand/image/analyzer/logic/opencv/OpenCvPlayground.java
+++ analyzer-logic/src/main/java/com/shtand/image/analyzer/logic/opencv/OpenCvPlayground.java
@@ -1,5 +1,9 @@
 package com.shtand.image.analyzer.logic.opencv;
 
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.objdetect.CascadeClassifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -8,12 +12,28 @@ import org.springframework.stereotype.Component;
 public class OpenCvPlayground {
 
     private static final Logger log = LoggerFactory.getLogger(OpenCvPlayground.class);
+    private static final String haarCascade = "/classifiers/haarcascade_frontalface_alt.xml";
+
+    private CascadeClassifier faceCascade;
 
     public OpenCvPlayground() {
         log.warn("Init openCV playground");
         nu.pattern.OpenCV.loadShared();
         System.loadLibrary(org.opencv.core.Core.NATIVE_LIBRARY_NAME);
         log.warn("openCV playground loaded");
+        faceCascade = loadHaarcascade();
     }
 
+    private CascadeClassifier loadHaarcascade() {
+        CascadeClassifier cascadeClassifier = new CascadeClassifier();
+        String file = OpenCvPlayground.class.getResource(haarCascade).getFile();
+        cascadeClassifier.load(file);
+        log.warn("haarCascade loaded");
+        return cascadeClassifier;
+    }
+
+    public Mat loadImageToMatrix(byte[] bytes) {
+        Mat mat = Imgcodecs.imdecode(new MatOfByte(bytes), Imgcodecs.CV_LOAD_IMAGE_UNCHANGED);
+        return mat;
+    }
 }
